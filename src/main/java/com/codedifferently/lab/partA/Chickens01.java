76d02d8c7369c9420d34af35041dd69a7050fefb
Angel Robles-Ortiz <angelroblesortiz.26@gmail.com>
2025-01-02 10:03:40 -0500

    submitting numerical data
    submission

--- src/main/java/com/codedifferently/lab/partA/Chickens01.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens01.java
@@ -2,6 +2,25 @@ package com.codedifferently.lab.partA;
 
 public class Chickens01 {
     //Put your code here
+    public static void main(String args[]) {
+        //3 chickens, 5 eggs per
+        int chickenCount = 3;
+        int eggsPerChicken = 5;
+        int totalEggs = 0;
 
-    //System.out.println(totalEggs);
+        chickenCount += 1;
+        chickenCount = chickenCount / 2;
+        totalEggs = chickenCount * eggsPerChicken;
+        System.out.println("Total Eggs: " + totalEggs);
+
+        //8 chickens, 4 eggs per
+        int chickenCount2 = 8;
+        int eggsPerChicken2 = 4;
+        int totalEggs2 = 0;
+
+        chickenCount2 += 1;
+        chickenCount2 = chickenCount2 / 2;
+        totalEggs2 = chickenCount2 * eggsPerChicken2;
+        System.out.println("Total Eggs: " + totalEggs2);
+    }
 }
--- src/main/java/com/codedifferently/lab/partA/Chickens02.java
+++ src/main/java/com/codedifferently/lab/partA/Chickens02.java
@@ -3,9 +3,14 @@ package com.codedifferently.lab.partA;
 public class Chickens02 {
     public static void main(String[] args) {
         //Put your code here
+        int totalEggs = 100 + 121 + 117;
+        int dailyAverage = totalEggs / 3;
+        System.out.println("Daily Average:   " + dailyAverage);
 
-        //System.out.println("Daily Average:   " +dailyAverage);
-        //System.out.println("Monthly Average: " +monthlyAverage);
-        //System.out.println("Monthly Profit:  $" +monthlyProfit);
+        int monthlyAverage = dailyAverage * 30;
+        System.out.println("Monthly Average: " + monthlyAverage);
+
+        double monthlyProfit = monthlyAverage * 0.18;
+        System.out.println("Monthly Profit:  $" + monthlyProfit);
     }
 }
